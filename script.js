78166480893a72738411b56aa63731e86c67159b
Katherine Jones <kejone1414@gmail.com>
2020-05-07 23:55:58 -0400

    updated portfolio

--- script.js
+++ script.js
@@ -33,50 +33,56 @@ $(document).ready(function () {
   })
 
   // objects to populate modals
+  const employeeObj = {
+    gif: "./assets/images/employeegif.gif",
+    description: "A Node CLI that prompts the user for information about employees, and generates an HTML file that displays summaries for each person.  Built using Node.js, JavaScript, HTML, and CSS.",
+    repo: "https://github.com/katjones23/EmployeeTemplate"
+  };
+  
   const readmeObj = {
     gif: "./assets/images/readmegif.gif",
-    description: "A generator for a markdown README file for GitHub users.",
+    description: "A generator for a markdown README file for GitHub users.  Built using Node.js, JavaScript, and Markdown.",
     repo: "https://github.com/katjones23/READMEGenerator"
   };
 
   const rtSiteObj = {
     gif: "./assets/images/rtsite.gif",
-    description: "A portfolio site built using HTML, CSS, and Javascript.",
+    description: "A portfolio site built using HTML, CSS, and JavaScript.",
     url: "https://profryanthompson.com/",
     repo: "https://github.com/katjones23/Dr-Ryan-Thompson-Webpage"
   };
 
   const n2moviesObj = {
     gif: "./assets/images/n2movies.gif",
-    description: "A web application that populates movie recommendations based off of a movie name.  Built with HTML, CSS, and Javascript, using data from APIs.",
+    description: "A web application that populates movie recommendations based off of a movie name.  Built with HTML, CSS, and JavaScript, using data from APIs.",
     url: "https://katjones23.github.io/N2Movies/",
     repo: "https://github.com/katjones23/N2Movies"
   };
 
   const weatherAppObj = {
     gif: "./assets/images/weatherapp.gif",
-    description: "A dynamic weather app that allows users to search cities, see search history, quickly redo previous searches, and see current weather and a five-day forecast.  Built with HTML, CSS, and Javascript, using data from an API.",
+    description: "A dynamic weather app that allows users to search cities, see search history, quickly redo previous searches, and see current weather and a five-day forecast.  Built with HTML, CSS, and JavaScript, using data from an API.",
     url: "https://katjones23.github.io/WeatherApp/",
     repo: "https://github.com/katjones23/WeatherApp"
   };
 
   const dayPlannerObj = {
     gif: "./assets/images/dayplanner.gif",
-    description: "A day planner app for work hours 8-5 to enter and save events for the same day.  Built with HTML, CSS, and Javascript.",
+    description: "A day planner app for work hours 8-5 to enter and save events for the same day.  Built with HTML, CSS, and JavaScript.",
     url: "https://katjones23.github.io/DayPlanner/",
     repo: "https://github.com/katjones23/DayPlanner"
   };
 
   const jsQuizObj = {
     gif: "./assets/images/quiz.gif",
-    description: "A short quiz of multiple choice questions to test your JavaScript knowledge.  Built with HTML, CSS, and Javascript.",
+    description: "A short quiz of multiple choice questions to test your JavaScript knowledge.  Built with HTML, CSS, and JavaScript.",
     url: "https://katjones23.github.io/JavaScriptQuiz/",
     repo: "https://github.com/katjones23/JavaScriptQuiz"
   };
 
   const passGenObj = {
     gif: "./assets/images/passwordgen.gif",
-    description: "A webpage with a password generator that will take input from the user as to length and character types to include in the password.  Built with HTML, CSS, and Javascript.",
+    description: "A webpage with a password generator that will take input from the user as to length and character types to include in the password.  Built with HTML, CSS, and JavaScript.",
     url: "https://katjones23.github.io/KJ-password-generator/",
     repo: "https://github.com/katjones23/KJ-password-generator"
   };
@@ -131,6 +137,11 @@ $(document).ready(function () {
       url.css("display", "block");
       url.attr("href", passGenObj.url);
       repo.attr("href", passGenObj.repo);
+    } else if ($(imgClicked).data("modal") === "Employee Template") {
+      gif.attr("src", employeeObj.gif);
+      description.text(employeeObj.description);
+      url.css("display", "none");
+      repo.attr("href", employeeObj.repo);
     } else if ($(imgClicked).data("modal") === "README Generator") {
       gif.attr("src", readmeObj.gif);
       description.text(readmeObj.description);
