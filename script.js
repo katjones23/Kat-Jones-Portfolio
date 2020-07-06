2c441d142a0909b16ab0bfb1b9b19721e292052e
Katherine Jones <kejone1414@gmail.com>
2020-07-06 17:23:24 -0400

    updated portfolio

--- script.js
+++ script.js
@@ -47,12 +47,6 @@ $(document).ready(function () {
     repo: "https://github.com/katjones23/NoteTaker"
   };
 
-  const employeeObj = {
-    gif: "./assets/images/employeegif.gif",
-    description: "A Node CLI that prompts the user for information about employees, and generates an HTML file that displays summaries for each person.  Built using Node.js, JavaScript, HTML, and CSS.",
-    repo: "https://github.com/katjones23/EmployeeTemplate"
-  };
-
   const readmeObj = {
     gif: "./assets/images/readmegif.gif",
     description: "A generator for a markdown README file for GitHub users.  Built using Node.js, JavaScript, and Markdown.",
@@ -71,34 +65,6 @@ $(document).ready(function () {
     description: "A collaboratively developed web application that populates movie recommendations based off of a movie name.  Built with HTML, CSS, and JavaScript, using data from APIs.",
     url: "https://katjones23.github.io/N2Movies/",
     repo: "https://github.com/katjones23/N2Movies"
-  };
-
-  const weatherAppObj = {
-    gif: "./assets/images/weatherapp.gif",
-    description: "A dynamic weather app that allows users to search cities, see search history, quickly redo previous searches, and see current weather and a five-day forecast.  Built with HTML, CSS, and JavaScript, using data from an API.",
-    url: "https://katjones23.github.io/WeatherApp/",
-    repo: "https://github.com/katjones23/WeatherApp"
-  };
-
-  const dayPlannerObj = {
-    gif: "./assets/images/dayplanner.gif",
-    description: "A day planner app for work hours 8-5 to enter and save events for the same day.  Built with HTML, CSS, and JavaScript.",
-    url: "https://katjones23.github.io/DayPlanner/",
-    repo: "https://github.com/katjones23/DayPlanner"
-  };
-
-  const jsQuizObj = {
-    gif: "./assets/images/quiz.gif",
-    description: "A short quiz of multiple choice questions to test your JavaScript knowledge.  Built with HTML, CSS, and JavaScript.",
-    url: "https://katjones23.github.io/JavaScriptQuiz/",
-    repo: "https://github.com/katjones23/JavaScriptQuiz"
-  };
-
-  const passGenObj = {
-    gif: "./assets/images/passwordgen.gif",
-    description: "A webpage with a password generator that will take input from the user as to length and character types to include in the password.  Built with HTML, CSS, and JavaScript.",
-    url: "https://katjones23.github.io/KJ-password-generator/",
-    repo: "https://github.com/katjones23/KJ-password-generator"
   };
 
   // function to populate modals
@@ -127,41 +93,12 @@ $(document).ready(function () {
       url.css("display", "block");
       url.attr("href", n2moviesObj.url);
       repo.attr("href", n2moviesObj.repo);
-    } else if ($(imgClicked).data("modal") === "Weather App") {
-      gif.attr("src", weatherAppObj.gif);
-      description.text(weatherAppObj.description);
-      url.css("display", "block");
-      url.attr("href", weatherAppObj.url);
-      repo.attr("href", weatherAppObj.repo);
-    } else if ($(imgClicked).data("modal") === "Day Planner") {
-      gif.attr("src", dayPlannerObj.gif);
-      description.text(dayPlannerObj.description);
-      url.css("display", "block");
-      url.attr("href", dayPlannerObj.url);
-      repo.attr("href", dayPlannerObj.repo);
-    } else if ($(imgClicked).data("modal") === "JavaScript Quiz") {
-      gif.attr("src", jsQuizObj.gif);
-      description.text(jsQuizObj.description);
-      url.css("display", "block");
-      url.attr("href", jsQuizObj.url);
-      repo.attr("href", jsQuizObj.repo);
-    } else if ($(imgClicked).data("modal") === "Password Generator") {
-      gif.attr("src", passGenObj.gif);
-      description.text(passGenObj.description);
-      url.css("display", "block");
-      url.attr("href", passGenObj.url);
-      repo.attr("href", passGenObj.repo);
     } else if ($(imgClicked).data("modal") === "Note Taker") {
       gif.attr("src", noteObj.gif);
       description.text(noteObj.description);
       url.css("display", "block");
       url.attr("href", noteObj.url);
       repo.attr("href", noteObj.repo);
-    } else if ($(imgClicked).data("modal") === "Employee Template") {
-      gif.attr("src", employeeObj.gif);
-      description.text(employeeObj.description);
-      url.css("display", "none");
-      repo.attr("href", employeeObj.repo);
     } else if ($(imgClicked).data("modal") === "README Generator") {
       gif.attr("src", readmeObj.gif);
       description.text(readmeObj.description);
@@ -175,5 +112,4 @@ $(document).ready(function () {
       repo.attr("href", recipeObj.repo);
     };
   });
-  recipeObj
 });  
